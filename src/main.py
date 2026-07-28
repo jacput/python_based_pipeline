899282bbf65a695b4b866b40dddbaebaa56fe0c7
jacput <jacob.puthiamadathil@gmail.com>
2026-07-28 14:40:35 -0400

    Updated Day 2 Added Pathlib

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import sys
 import logging
 from datetime import date
 import json
+from pathlib import Path
 
 todays_date = date.today().strftime("%m%d%Y")
 # Open the config file and parse its contents
@@ -14,11 +15,11 @@ try:
 except Exception as e:
     sys.exit(1)
 else:
-    source_file_path = config_data.get('source_file_path') 
+    source_file_path = Path(config_data.get('source_file_path'))
     source_file_name = config_data.get('source_file_name')
-    log_file_path = config_data.get('logging_file_full_path')
+    log_file_path = Path(config_data.get('logging_file_full_path') + f"{todays_date}.log")
     logging.basicConfig(
-    filename= log_file_path + f"_{todays_date}.log",
+    filename= log_file_path,
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
@@ -29,17 +30,17 @@ else:
     logger.info("source_file_name: %s", source_file_name)
     logger.info("log_file_path: %s", log_file_path)
     try:
-        source_file_full_path = f"{source_file_path}{source_file_name}"
-        source_file_with_date = source_file_full_path + f"{todays_date}.csv"
+        source_file_full_path = source_file_path / source_file_name
+        source_file_with_date = source_file_full_path.with_name(f"{source_file_full_path.stem}{todays_date}.csv")
         logger.info("Starting to load the CSV file.")
         file_path = source_file_with_date
         df = pd.read_csv(file_path)
         logger.info("CSV file loaded successfully.  Location of the file: %s", file_path)
     except FileNotFoundError:
-        logger.error("File not found: %s", file_path)
+        logger.exception("File not found: %s", file_path)
         sys.exit(1)
     except Exception as e:
-        logger.error("Error loading file: %s", e)
+        logger.exception("Error loading file: %s", e)
         sys.exit(1)
     else:
         required_columns = [
@@ -55,13 +56,13 @@ else:
         missing_columns = set(required_columns) - set(df.columns)
 
         if missing_columns:
-            logger.error("Missing columns: %s", missing_columns)
+            logger.exception("Missing columns: %s", missing_columns)
             sys.exit(1)
 
         # View the first 5 rows
-        print(df.head())
+        logger.info("First 5 rows of the DataFrame:")
+        logger.info(df.head())
         row_count = len(df)
-        print(f"Number of rows: {len(df)}")
         logger.info("Number of rows in the DataFrame: %d", row_count)
         logger.info("File loaded successfully with all required columns.")
         sys.exit(0)  # Graceful success exit
